590ea8b4d82d6b01c534098934503c175aa470f7
Daniel4x <Daniel4x@gmail.com>
2016-07-24 10:02:48 +0300

    Added WebDriverIO code sample

---
+++ Selenium/WebDriverIO/webdriverio.js
@@ -0,0 +1,26 @@
+var webdriverio = require('webdriverio');
+var options = {
+        desiredCapabilities: {
+             platformName: 'ANDROID',                       
+             browserName: 'mobileOS',                                             
+             //deviceName: 'yourDeviceID',
+             user: 'MY_USER@perfectomobile.com',
+             password:'MY_PASS',
+    },
+    host: 'demo.perfectomobile.com',
+    path: '/nexperience/perfectomobile/wd/hub',
+port:80 
+};
+ 
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+	.setValue('#lst-ib' , 'PerfectoCode GitHub')
+	.click('#tsbb')
+	.click('#rso > div > div:nth-child(1) > div > div > div._OXf > h3 > a')
+    .getTitle().then(function(title) { //Title 
+        console.log('Title was: ' + title);
+    })
+    .end();
+	
