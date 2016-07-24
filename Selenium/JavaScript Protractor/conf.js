00e642e4b84e44ec727fcdbf70e1179a281cb13d
Daniel <daniel4x@gmail.com>
2016-07-24 17:36:44 +0300

    Update conf.js

--- Selenium/JavaScript Protractor/conf.js
+++ Selenium/JavaScript Protractor/conf.js
@@ -1,29 +1,29 @@
 // An example configuration file.
 
 exports.config = {
-  //Remote address																						   
-  seleniumAddress: 'https://MY_HOST.perfectomobile.com/nexperience/perfectomobile/wd/hub',
+	//Remote address																						   
+	seleniumAddress: 'https://MY_HOST.perfectomobile.com/nexperience/perfectomobile/wd/hub',
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    browserName: 'chrome',
-  	user: 'MY_USER',
-    password: 'MY_PASS',
-	  platformName: 'Android',
-    //deviceName: '123456',
-  }, 
+  	//Capabilities to be passed to the webdriver instance.
+	capabilities: {
+		browserName: 'chrome',
+		user: 'MY_USER',
+		password: 'MY_PASS',
+		platformName: 'Android',
+		//deviceName: '123456',
+  	}, 
 
-  // Framework to use. Jasmine is recommended.
-  framework: 'jasmine',
+  	//Framework to use. Jasmine is recommended.
+  	framework: 'jasmine',
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: ['spec.js'],
+  	// Spec patterns are relative to the current working directly when
+  	// protractor is called.
+  	specs: ['spec.js'],
   
-  // Options to be passed to Jasmine.
-  jasmineNodeOpts: {
-	showColors: true,	// Use colors in the command line report.	
-    defaultTimeoutInterval: 120000	// Time to wait in ms before a test fails. Default value = 30000
-  }
+	// Options to be passed to Jasmine.
+	jasmineNodeOpts: {
+		showColors: true,	// Use colors in the command line report.	
+    	defaultTimeoutInterval: 120000	// Time to wait in ms before a test fails. Default value = 30000
+  	}
 }
 
